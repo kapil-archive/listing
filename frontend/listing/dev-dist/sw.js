028aeb699451375a3f02ab90989563072f889101
Kapil <kotlin.archive@gmail.com>
2026-04-08 16:04:40 +0530

    PORT updated

--- frontend/listing/dev-dist/sw.js
+++ frontend/listing/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.6iuq861jgb4"
+    "revision": "0.ion2g50ac4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
