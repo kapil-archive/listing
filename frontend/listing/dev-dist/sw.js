5970a8be4e0b0a185ccebdea8e1fce6a70e202fe
Kapil <kotlin.archive@gmail.com>
2026-04-14 14:39:56 +0530

    changes

--- frontend/listing/dev-dist/sw.js
+++ frontend/listing/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.sekp8hcc64o"
+    "revision": "0.6hde53h364s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
